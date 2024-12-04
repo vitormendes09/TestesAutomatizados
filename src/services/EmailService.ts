cfe1b4cd0fb81d186c4a7e3f9cdf843419ff57ae
Vitor Mendes <mendes.v@gsuite.iff.edu.br>
2024-12-03 21:24:48 -0300

    Criado uma interface para a classe EmailService

--- src/services/EmailService.ts
+++ src/services/EmailService.ts
@@ -13,7 +13,12 @@ interface IMessageDTO{
     to: IMailTo;
     message: IMailMessage;
 }
-class EmailService{
+
+interface IEmailService{
+
+    sendMail(request: IMessageDTO): void;
+}
+class EmailService implements IEmailService{
 
     sendMail({to, message}: IMessageDTO){
         console.log(`Email enviado para ${to.name}: ${message.subject}`);
